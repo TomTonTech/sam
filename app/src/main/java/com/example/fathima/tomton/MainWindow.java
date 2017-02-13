82a0f393ad8e90b2320118d81c61cb943cde778a
absouban354 <absouban354@gmail.com>
2017-02-14 00:58:33 +0530

    Added Sqlite Database In TeacherLogin.java   : removed log verbose   : added exception handling In MainActivity.java   : removed prioirty variable In MainWindow.java   : added sqlite sync function

--- app/src/main/java/com/example/fathima/tomton/MainWindow.java
+++ app/src/main/java/com/example/fathima/tomton/MainWindow.java
@@ -257,6 +257,7 @@ public class MainWindow extends AppCompatActivity {
                 String result=bufferedReader.readLine();
                 if(result.equalsIgnoreCase("same"))
                     return "same";
+                
                 else {
                     try {
                         URL url1 = new URL("http://192.168.1.100/apk/sam.apk");
