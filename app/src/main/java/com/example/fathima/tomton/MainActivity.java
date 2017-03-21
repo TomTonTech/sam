f9af90019e4f8cd4099a3f525d33ac6a5fe959b4
anbk77 <ahammednafihbk@gmail.com>
2017-03-21 23:43:30 +0530

    project pre

--- app/src/main/java/com/example/fathima/tomton/MainActivity.java
+++ app/src/main/java/com/example/fathima/tomton/MainActivity.java
@@ -28,7 +28,7 @@ import java.net.URL;
 public class MainActivity extends AppCompatActivity {
 
     // CONNECTION_TIMEOUT and READ_TIMEOUT are in milliseconds
-    public static final String URL_ADDR="http://192.168.1.100/android/";
+    public static final String URL_ADDR="http://10.0.2.2/android1/";
     public static final String APK_ADDR="http://192.168.1.100/apk/";
     public static final int CONNECTION_TIMEOUT=10000;
     public static final int READ_TIMEOUT=15000;
